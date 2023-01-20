3125b09faf370d22b4133246804bf4956611ccd7
fabou78 <fabou78@example.com>
2023-01-20 20:08:07 +0000

    Rest Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,16 @@
-from django.http import JsonResponse
-
 from django.forms.models import model_to_dict
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
 
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
+    '''
+    DRF API View
+    '''
     # request -> HttpRequest from Django
     # print(dir(request))
     # request.body
@@ -22,4 +27,4 @@ def api_home(request, *args, **kwargs):
     #     data['content'] = model_data.content
     #     data['price'] = model_data.price
 
-    return JsonResponse(data)
+    return Response(data)
