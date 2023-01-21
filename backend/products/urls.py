6e739da34b600133a46f56c0d8337b83e5bc034c
fabou78 <fabou78@example.com>
2023-01-21 14:36:02 +0000

    Django Rest Framework ListAPIView & ListCreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     # as_view() not required below because configured in views.py
-    path('', views.product_create_view),
+    path('', views.product_list_create_view),
     # The / 'slash at the end is important
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -4,15 +4,36 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 
 # another way to form the url (see products.urls.py)
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductListAPIView(generics.ListAPIView):
+    '''
+    We are not using this method because we use the combined
+    listcreate view above
+    '''
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+product_list_view = ProductListAPIView.as_view()
