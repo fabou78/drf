d6ba787ca03bb70fb9ab9c44e22dfe445048af93
fabou78 <fabou78@example.com>
2023-01-21 14:11:40 +0000

    Django Rest Framework CreateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # The / 'slash is important
+    # as_view() not required below because configured in views.py
+    path('', views.product_create_view),
+    # The / 'slash at the end is important
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -4,6 +4,15 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+# another way to form the url (see products.urls.py)
+product_create_view = ProductCreateAPIView.as_view()
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
