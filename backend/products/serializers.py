aecef3a664f2c39f23c11c4b93d3332c0a46ffcf
fabou78 <fabou78@example.com>
2023-01-22 13:28:33 +0000

    URLs, Reverse, & Serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 
 from .models import Product
 
@@ -7,11 +8,22 @@ class ProductSerializer(serializers.ModelSerializer):
     # Below we are renaming the product model's property from get_discount to
     # my_discount
     my_discount = serializers.SerializerMethodField(read_only=True)
+    detail_url = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    # Below is another example using HyperlinkedIdentityField which
+    # will only work with ModelSerializer and also not need the
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+    )
 
     class Meta:
         model = Product
         fields = [
             'id',
+            'detail_url',
+            'edit_url',
+            'url',
             'title',
             'content',
             'price',
@@ -19,5 +31,29 @@ class ProductSerializer(serializers.ModelSerializer):
             'my_discount',
         ]
 
+    def get_detail_url(self, obj):
+        request = self.context.get('request')  # self.request not usable here
+        if request is None:
+            return None
+        return reverse(
+            'product-detail',
+            kwargs={'pk': obj.pk},
+            request=request,
+        )
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')  # self.request not usable here
+        if request is None:
+            return None
+        return reverse(
+            'product-edit',
+            kwargs={'pk': obj.pk},
+            request=request,
+        )
+
     def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,10 +4,14 @@ from . import views
 
 urlpatterns = [
     # as_view() not required below because configured in views.py
-    path('', views.product_list_create_view),
+    path('', views.product_list_create_view, name='product-list'),
     # path('', views.product_mixin_view),
     # The / 'slash at the end is important
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
-    path('<int:pk>/update/', views.product_update_view),
+    path(
+        '<int:pk>/',
+        views.ProductDetailAPIView.as_view(),
+        name='product-detail',
+    ),
+    path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     path('<int:pk>/delete/', views.product_delete_view),
 ]
